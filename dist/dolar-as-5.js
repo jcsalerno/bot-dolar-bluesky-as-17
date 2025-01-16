e11fccfaa3c3ec54ebfd83807b1dcec26e4323ce
jcsalerno <contato.jcsalerno@gmail.com>
2025-01-15 21:15:08 -0300

    fix: correção para deploy na render.com

--- dist/dolar-as-5.js
+++ dist/dolar-as-5.js
@@ -1,12 +1,50 @@
-import { BskyAgent } from '@atproto/api';
-import * as dotenv from 'dotenv';
-import fetch from 'node-fetch';
-import cron from 'node-cron';
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const api_1 = require("@atproto/api");
+const dotenv = __importStar(require("dotenv"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+const node_cron_1 = __importDefault(require("node-cron"));
 dotenv.config();
 // Função para obter o valor do dólar
 async function fetchDollarValue() {
     try {
-        const response = await fetch(process.env.DOLLAR_API_URL);
+        const response = await (0, node_fetch_1.default)(process.env.DOLLAR_API_URL);
         // Tentar converter a resposta JSON em um tipo conhecido
         const data = await response.json();
         // Verificar se a resposta possui a estrutura correta
@@ -26,7 +64,7 @@ async function fetchDollarValue() {
 }
 // Função para fazer o post no BlueSky
 async function postToBlueSky(postMessage) {
-    const agent = new BskyAgent({
+    const agent = new api_1.BskyAgent({
         service: 'https://bsky.social',
     });
     try {
@@ -65,7 +103,7 @@ async function main() {
     }
 }
 // Agendando a execução do cron job todos os dias às 17h
-cron.schedule('0 17 * * *', async () => {
+node_cron_1.default.schedule('0 17 * * *', async () => {
     try {
         await main();
     }
